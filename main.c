1c0f60b40e0ab21d6e91a6001690351ff60558ff
Sergey Litvinov <slitvinov@gmail.com>
2022-08-30 23:30:16 +0200

    up

--- main.c
+++ main.c
@@ -5,44 +5,53 @@
 #include "force.h"
 
 static void
-force(struct params *C, int m, real *r0, int *connect, real *r, real *f)
+force(struct params *C, int n, real *r, real *f)
 {
-	int q;
 	int i;
 	int j;
+	int k;
+	int l;
 
-	for (q = 0; q  < m; q++) {
-		i = connect[2* q];
-		j = connect[2 * q + 1];
-		fbond0(C->kb, r0[q], r[3*i], r[3*i + 1], r[3*i + 2], r[3*j], r[3*j + 1], r[3*j + 2],
-							&f[3*i], &f[3*i + 1], &f[3*i + 2], &f[3*j], &f[3*j + 1], &f[3*j + 2]);
+	for (i = 0; i  < n - 1; i++) {
+		j = i + 1;
+		fbond0(C->kb, C->r0, r[3*i], r[3*i + 1], r[3*i + 2], r[3*j], r[3*j + 1], r[3*j + 2],
+		    &f[3*i], &f[3*i + 1], &f[3*i + 2], &f[3*j], &f[3*j + 1], &f[3*j + 2]);
 	}
 
+	for (i = 0; i < n - 2; i++) {
+		j = i + 1;
+		k = i + 2;
+		fangle0(C->kth, C->th0,  r[3*i], r[3*i + 1], r[3*i + 2],  r[3*j], r[3*j + 1], r[3*j + 2],  r[3*k], r[3*k + 1], r[3*k + 2],
+		    &f[3*i], &f[3*i + 1], &f[3*i + 2], &f[3*j], &f[3*j + 1], &f[3*j + 2], &f[3*k], &f[3*k + 1], &f[3*k + 2]);
+	}
+
+	for (i = 0; i < n - 3; i++) {
+		j = i + 1;
+		k = i + 2;
+		l = i + 3;
+		fdihedral0(C->kd, 
+			r[3*i], r[3*i + 1], r[3*i + 2], 
+			r[3*j], r[3*j + 1], r[3*j + 2], 
+			r[3*k], r[3*k + 1], r[3*k + 2],
+		    	r[3*l], r[3*l + 1], r[3*l + 2],
+		    &f[3*i], &f[3*i + 1], &f[3*i + 2], &f[3*j], &f[3*j + 1], &f[3*j + 2], &f[3*k], &f[3*k + 1], &f[3*k + 2], &f[3*l], &f[3*l + 1], &f[3*l + 2]);
+	}
 }
 
 int
 main(void)
 {
-	int c0;
-	int c1;
 	int i;
 	int j;
 	int n;
-	int m;
 	int N;
-	int M;
 	real *r;
 	real *f;
 	real t[3];
 	real dt;
-	real *r0;
-	real r00;
-	int *connect;
-	FILE *file;
-	char *path = "connect";
 
 	struct params C =  {
-		.kb = 1.0, .r0 = 1.0					};
+		.kb = 1.0, .r0 = 1.0, .kth = 0.01, .th0 = 160 *  3.141592 / 180, .kd = -1e-4		};
 	r = NULL;
 	n = N = 0;
 	while (scanf("%lf %lf %lf", &t[0], &t[1], &t[2]) == 3) {
@@ -59,37 +68,20 @@ main(void)
 		r[3 * n + 2] = t[2];
 		n++;
 	}
-	connect = NULL;
-	r0 = NULL;
-	file = fopen(path, "r");
-	if (file == NULL) {
-		fprintf(stderr, "fail to open '%s'\n", path);
+	srand(1234);
+	dt = 0.25;
+	f = malloc(3 * n * sizeof *f);
+	if (f == NULL) {
+		fprintf(stderr, "malloc failed\n");
 		exit(1);
 	}
-	m = M = 0;
-	while (fscanf(file, "%d %d %lf\n", &c0, &c1, &r00) == 3) {
-		if (m == M) {
-			M = 2 * n + 1;
-			connect = realloc(connect, 2 * M * sizeof *connect);
-			r0 = realloc(r0, M * sizeof *r0);
-		}
-		connect[2 * m] = c0;
-		connect[2 * m + 1] =c1;
-		r0[m] = r00;
-		m++;
-	}
-	fclose(file);
-	fprintf(stderr, "nm: %d %d\n", n, m);
-	srand(1234);
-	dt = 0.1;
-	f = malloc(3 * n * sizeof *f);
-	for (j = 0; j < 10000; j++) {
+	for (j = 0; j < 100000000; j++) {
 		for (i = 0; i < 3 * n; i++)
 			f[i] = 0;
-		force(&C, m, r0, connect, r, f);
+		force(&C,n, r, f);
 		for (i = 0; i < 3 * n; i++)
 			r[i] += dt * f[i];
-		if (j % 100 == 0) {
+		if (j % 100000 == 0) {
 			for (i = 0; i < n; i++)
 				printf("%.16e %.16e %.16e\n", r[3*i], r[3*i + 1], r[3*i + 2]);
 			printf("\n");
@@ -97,5 +89,4 @@ main(void)
 	}
 	free(r);
 	free(f);
-	free(connect);
 }
